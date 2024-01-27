a23cb18b3e6f245944adbc9b851245e80a7644d8
emfernandezv <36644789+emfernandezv@users.noreply.github.com>
2024-01-27 16:36:47 -0700

    robust error handler

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
@@ -50,15 +50,15 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
+    message,
     nav
-  });
-});
-
-
-/* ***********************
+  })
+})
+/* ******************+
+*****
  * Local Server Information
  * Values from .env (environment) file
  *************************/
